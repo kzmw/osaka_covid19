af2b75060009a816f6a7a0242bd047ea0c01b75d
kzmw <63456328+kzmw@users.noreply.github.com>
2020-04-17 23:12:03 +0900

    Update mapping.js

--- mapping.js
+++ mapping.js
@@ -11,7 +11,12 @@
         .attr("height", HEIGHT);
     var g = svg.append("g");
     d3.json('/osaka.geojson').then(function(json) {
-        var projection = d3.geoMercator()
+    d3.json('/data.json').then(function(data){
+for(var i = 0;i <= json.features.length -1; i++){
+    json.features[i].properties.color = "red"
+}
+    }
+	    var projection = d3.geoMercator()
             .scale(30000)
             .center(d3.geoCentroid(json))
             .translate([WIDTH / 2, HEIGHT / 2]);
@@ -22,13 +27,10 @@
             .enter()
             .append('path')
             .attr('d', path)
-            .attr('fill', "red")
+            .attr('fill', function(d){
+	return d.properties.color
+	})
 	    .attr('stroke', "white");
-	   function(d){
-	d3.json('/data.json').then(function(data) {
-        d.properties.color = "silver"
-        });
-	}; 
         g.selectAll('text')
             .data(json.features)
             .enter()
