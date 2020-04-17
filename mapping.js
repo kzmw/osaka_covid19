46be5078a11892736b835391ad8b833d2a63c6e0
kzmw <63456328+kzmw@users.noreply.github.com>
2020-04-18 01:16:32 +0900

    Update mapping.js

--- mapping.js
+++ mapping.js
@@ -15,9 +15,7 @@
 for(var i = 0;i <= json.features.length -1; i++){
 for(var j = 1;j<=data.city.length -1;j++){
 if(data.city[j].code == json.features[i].properties.N03_007){
-    if(data.city[j].patient == 3){
-    json.features[i].properties.color = "red"}
-	else{json.features[i].properties.color = "silver"}
+    json.features[i].properties.color = data.city[j].color[0]
 }}
 }
 	    var projection = d3.geoMercator()
