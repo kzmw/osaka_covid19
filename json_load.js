2062332c85af4f2124b684c0fc0f56fa968e0b58
kzmw <63456328+kzmw@users.noreply.github.com>
2020-04-18 15:26:28 +0900

    Update json_load.js

--- json_load.js
+++ json_load.js
@@ -1,7 +1,7 @@
 $.getJSON("/data.json", (data) => {
  $("#number_text").text(data.sum);
- $("#citizen").text(data.citizen);
- $("#change").text(data.change);
+ $("#citizen").text("うち大阪府民" + data.citizen + "人");
+ $("#change").text("前日比" + data.change + "人");
  $("#men").text("男性：" + data.sex[0] + "%");
  $("#women").text("女性：" + data.sex[1] + "%");
  $("#preschool").text("未就学児：" + data.age[0] + "人");
