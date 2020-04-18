15cebe8696df0562689d76d613a4958f4f984613
kzmw <63456328+kzmw@users.noreply.github.com>
2020-04-18 20:07:18 +0900

    Update mapping.js

--- mapping.js
+++ mapping.js
@@ -21,7 +21,7 @@ if(data.city[j].code == json.features[i].properties.N03_007){
 }
 	    var projection = d3.geoMercator()
             .scale(30000)
-            .center([200,300])
+            .center(d3.geoCentroid(json))
             .translate([WIDTH / 2, HEIGHT / 2]);
         var path = d3.geoPath()
             .projection(projection);
