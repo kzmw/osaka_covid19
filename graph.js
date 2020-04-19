3565e8a5792b188b6784299ec7c9b4238b582357
kzmw <63456328+kzmw@users.noreply.github.com>
2020-04-19 18:16:17 +0900

    Update graph.js

--- graph.js
+++ graph.js
@@ -1,13 +1,22 @@
-  // データ --- (*1)
-  const data = {
-    labels: ['A', 'B', 'C', 'D', 'E'],
-    datasets: [{
-      label: '国語のテスト',
-      data: [78, 64, 35, 90, 83]
-    }]}
-  // グラフを描画 --- (*2)
-  const ctx = document.getElementById('sex')
-  const chart_cv = new Chart(ctx, {
-    type: 'bar', // グラフの種類
-    data: data, // データ
-    options: {}}) // オプション
+window.onload = function() {
+var ctx = document.getElementById("sex");
+  var myPieChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: ["男性", "女性"],
+      datasets: [{
+          backgroundColor: [
+              "#BB5179",
+              "#FAFF67"
+          ],
+          data: [52, 48]
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: '男女比'
+      }
+    }
+  });
+  }
