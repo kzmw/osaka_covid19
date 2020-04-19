78f77f36f201466b2b7878083ca82f87031e777f
kzmw <63456328+kzmw@users.noreply.github.com>
2020-04-19 23:43:42 +0900

    Update graph.js

--- graph.js
+++ graph.js
@@ -23,7 +23,7 @@ var sex = document.getElementById("sex");
 
 var age = document.getElementById("age");
   var age_chart = new Chart(age, {
-    type: 'pie',
+    type: 'horizontalBar',
     data: {
       labels: ["未就学児", "就学児", "10代", "20代", "30代", "40代", "50代", "60代", "70代", "80代", "90代", "100代以上"],
       datasets: [{
@@ -44,7 +44,13 @@ var age = document.getElementById("age");
           data: [data.age[0], data.age[1],data.age[2],data.age[3],data.age[4],data.age[5],data.age[6],data.age[7],data.age[8],data.age[9],data.age[10],data.age[11]]
       }]
     },
-    options: {
+    options: {scales: {
+        xAxes: [{
+            ticks: {
+                min: 0
+            }
+        }]
+    },
       title: {
         display: true,
         text: '性別'
