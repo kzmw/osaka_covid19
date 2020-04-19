1c2abd1c6a5cb43f6b30ead7f48e459cb2467b30
kzmw <63456328+kzmw@users.noreply.github.com>
2020-04-19 18:22:47 +0900

    Update graph.js

--- graph.js
+++ graph.js
@@ -1,6 +1,6 @@
 window.onload = function() {
-var ctx = document.getElementById("sex");
-  var myPieChart = new Chart(ctx, {
+var sex = document.getElementById("sex");
+  var sex_graph = new Chart(sex, {
     type: 'pie',
     data: {
       labels: ["男性", "女性"],
@@ -16,6 +16,37 @@ var ctx = document.getElementById("sex");
       title: {
         display: true,
         text: '男女比'
+      }
+    }
+  });
+
+var age = document.getElementById("age");
+  var age_chart = new Chart(age, {
+    type: 'pie',
+    data: {
+      labels: ["未就学児", "就学児", "10代", "20代", "30代", "40代", "50代", "60代", "70代", "80代", "90代", "100代以上"],
+      datasets: [{
+          backgroundColor: [
+              "#BB5179",
+              "#FAFF67",
+              "#FAFF67",
+              "#FAFF67",
+              "#FAFF67",
+              "#FAFF67",
+              "#FAFF67",
+              "#FAFF67",
+              "#FAFF67",
+              "#FAFF67",
+              "#FAFF67",
+              "#FAFF67"
+          ],
+          data: [8, 6,33,255,202,209,172,102,108,59,8,1]
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: '性別'
       }
     }
   });
