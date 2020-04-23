ac9f90932f40a773c64c74a3c2d123b2f8679874
kzmw <63456328+kzmw@users.noreply.github.com>
2020-04-23 20:07:03 +0900

    Update graph.js

--- graph.js
+++ graph.js
@@ -31,6 +31,7 @@ var age = document.getElementById("age");
       labels: ["未就学児", "就学児", "10代", "20代", "30代", "40代", "50代", "60代", "70代", "80代", "90代", "100代以上"],
       datasets: [{
           label: '人数',
+backgroundColor: "#40e0d0",
           data: [data.age[0], data.age[1],data.age[2],data.age[3],data.age[4],data.age[5],data.age[6],data.age[7],data.age[8],data.age[9],data.age[10],data.age[11]]
       }]
     },
