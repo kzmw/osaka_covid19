c3358b148aff19361dcdce7deb3926e8dd73a197
kzmw <63456328+kzmw@users.noreply.github.com>
2020-04-23 21:44:10 +0900

    Update json_load.js

--- json_load.js
+++ json_load.js
@@ -3,7 +3,6 @@ $.getJSON("/data.json", (data) => {
 if(data.change > 0){$("#change").text("前日比：＋" + data.change + "人")}
 else if(data.change == 0){$("#change").text("前日比：±" + data.change + "人")}
 else if(data.change < 0){$("#change").text("前日比：－" + data.change + "人")}
-
 $(function table_generate(){
 var table_data = "";
 	for (var i=0;i<=(data.city.length -1);i++){
@@ -17,8 +16,12 @@ change =  "±" + Number(change).toLocaleString()
 else if(change < 0){
 change = "－" + Number(change).toLocaleString()
 }
+	if( data.city[i].name == "大阪府外"){
 	table_data += "<tr><td>" + data.city[i].name + "</td><td style=\"text-align: right\">" + Number(data.city[i].population).toLocaleString() + "</td><td style=\"text-align: right\">" + Number(data.city[i].patient).toLocaleString() + "</td><td style=\"text-align: right\">" + change + "</td><td>" + data.city[i].proportion + "</td></tr>";
 	}
+	else{
+        table_data += "<tr><td>" + data.city[i].name + "</td><td style=\"text-align: right\"></td><td style=\"text-align: right\">" + Number(data.city[i].patient).toLocaleString() + "</td><td style=\"text-align: right\">" + change + "</td><td></td></tr>";
+	}}
 table_data = "<tr><th>居住地</th><th>人口</th><th>感染者数</th><th>前日比</th><th>感染者の割合</th></tr>" + table_data;
 $("#covid_table").html(table_data)
 });
