921d80f2b59e90c1040c351c58dc46a6528da4db
kzmw <63456328+kzmw@users.noreply.github.com>
2020-04-23 19:58:49 +0900

    Update graph.js

--- graph.js
+++ graph.js
@@ -1,7 +1,7 @@
 $.getJSON("/data.json", (data) => {
 Chart.defaults.global. defaultFontFamily = "'Noto Sans JP', sans-serif";
-
 Chart.defaults.global.defaultFontColor = 'Black';
+Chart.defaults.global.defaultColor = rgb(64,224,208)
 
 var sex = document.getElementById("sex");
   var sex_graph = new Chart(sex, {
@@ -31,9 +31,6 @@ var age = document.getElementById("age");
       labels: ["未就学児", "就学児", "10代", "20代", "30代", "40代", "50代", "60代", "70代", "80代", "90代", "100代以上"],
       datasets: [{
           label: '人数',
-          backgroundColor: [
-              "#BB5179"
-          ],
           data: [data.age[0], data.age[1],data.age[2],data.age[3],data.age[4],data.age[5],data.age[6],data.age[7],data.age[8],data.age[9],data.age[10],data.age[11]]
       }]
     },
