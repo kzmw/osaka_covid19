22af54e717921d9ccb683df025854ecc04cad826
kzmw <63456328+kzmw@users.noreply.github.com>
2020-04-26 18:59:03 +0900

    Update json_load.js

--- json_load.js
+++ json_load.js
@@ -1,5 +1,6 @@
 $.getJSON("/data.json", (data) => {
  $("#number_text").text(data.sum);
+ $("#update").text(data.update);
 if(data.change > 0){$("#change").text("前日比：＋" + data.change + "人")}
 else if(data.change == 0){$("#change").text("前日比：±" + data.change + "人")}
 else if(data.change < 0){$("#change").text("前日比：－" + data.change + "人")}
