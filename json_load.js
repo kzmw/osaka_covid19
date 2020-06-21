6e60b9ac91b9fd545f48087644e25afd73623a5b
kzmw <63456328+kzmw@users.noreply.github.com>
2020-06-22 07:35:59 +0900

    Update json_load.js

--- json_load.js
+++ json_load.js
@@ -6,7 +6,7 @@ window.onload = function(){
 function getJsonp_GAS() {
 	$.ajax({
 		type: 'GET',
-		url: 'https://script.google.com/macros/s/xxxxxxxxxxxxxxxxxxxx/exec',
+		url: 'https://script.google.com/macros/s/AKfycbyrNVl5HtIe6be_n_d2ixxEG5ABXkTb_bZI3RvfOM4MpAXYySQ/exec',
 		dataType: 'jsonp',
 		jsonpCallback: 'jsondata',
 		success: function(data){
