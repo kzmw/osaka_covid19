fd2b774f5cd05805823fbafc127a43e3212609cd
kzmw <63456328+kzmw@users.noreply.github.com>
2020-06-22 01:08:21 +0900

    Update json_load.js

--- json_load.js
+++ json_load.js
@@ -4,7 +4,7 @@ function getJsonp_GAS() {
 		url: 'https://script.google.com/macros/s/xxxxxxxxxxxxxxxxxxxx/exec',
 		dataType: 'jsonp',
 		jsonpCallback: 'jsondata',
-		success: function(json){
+		success: function(data){
         var len = data.length;
         $("#number_text").text(len);
         $("#update").text("最終更新：更新停止中");
