fed92bff96d0c135345ef557303d2e39ffd9dff9
kzmw <63456328+kzmw@users.noreply.github.com>
2020-07-19 00:10:40 +0900

    Update json_load.js

--- json_load.js
+++ json_load.js
@@ -3,9 +3,7 @@ window.onload = function(){
 }
 
 function getJsonp_GAS() {
-fetch('https://script.google.com/macros/s/AKfycbyrNVl5HtIe6be_n_d2ixxEG5ABXkTb_bZI3RvfOM4MpAXYySQ/exec', {
-mode: 'no-cors'
-})
+fetch('https://script.google.com/macros/s/AKfycbyrNVl5HtIe6be_n_d2ixxEG5ABXkTb_bZI3RvfOM4MpAXYySQ/exec')
 .then( response => response.json())
 .then( jsonData => {
 var len = jsonData.length;
