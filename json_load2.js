d6f8980d414b4d1d048b3af8d0833102dd3da0c9
kzmw <63456328+kzmw@users.noreply.github.com>
2020-07-24 12:19:53 +0900

    Update json_load2.js

--- json_load2.js
+++ json_load2.js
@@ -45,8 +45,20 @@ if (data.city[i].name == "大阪府外" || data.city[i].name == "不明") {
 table_data = "<tr><th>居住地</th><th>人口</th><th>感染者数</th><th>前日比</th><th>感染者の割合<div class=\"proportion_select\"><input type=\"radio\" name=\"proportion1\" id=\"person\" value=\"人\" checked=\"\" onclick=\"person()\"><label for=\"person\" class=\"proportion_person\">人</label><input type=\"radio\" id=\"percent\" name=\"proportion1\" value=\"%\" onclick=\"percent()\"><label for=\"percent\" class=\"proportion_percent\">%</label></div></th><th style=\"display:none\">感染者の割合<div class=\"proportion_select\"><input type=\"radio\" name=\"proportion2\" id=\"person\" value=\"人\" onclick=\"person()\"><label for=\"person\" class=\"proportion_person\">人</label><input type=\"radio\" id=\"percent\" name=\"proportion2\" value=\"%\" checked=\"\" onclick=\"percent()\"><label for=\"percent\" class=\"proportion_percent\">%</label></div></th></tr>" + table_data;
 $("#covid_table").html(table_data)
 });
+var defaultFontColor = 'Black';
+var gridColor = 'rgba(0,0,0,0.1)';
+var zeroLineColor = 'rgba(0,0,0,0.25)';
+var borderColor = 'White';
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches == true){
+defaultFontColor = 'White';
+gridColor = 'rgba(0,0,0,0.9)';
+zeroLineColor = 'rgba(0,0,0,0.75)';
+borderColor = 'Black';
+}
+
 Chart.defaults.global.defaultFontFamily = "'Noto Sans JP', sans-serif";
-Chart.defaults.global.defaultFontColor = 'Black';
+Chart.defaults.global.defaultFontColor = defaultFontColor;
 Chart.defaults.global.title.fontSize = 18;
 
 var sex = document.getElementById("sex");
@@ -59,7 +71,8 @@ var sex = document.getElementById("sex");
               "#1e90ff",
               "#ff69b4"
           ],
-          data: [data.sex[0], data.sex[1]]
+          borderColor:borderColor,
+          data: [male, female]
       }]
     },
     options: {
@@ -78,14 +91,24 @@ var age = document.getElementById("age");
       datasets: [{
           label: '人数',
 backgroundColor: "#40e0d0",
-          data: [data.age[0], data.age[1],data.age[2],data.age[3],data.age[4],data.age[5],data.age[6],data.age[7],data.age[8],data.age[9],data.age[10],data.age[11]]
+          data: [preschool, school,teens,twenties,thirties,fourties,fifties,sixties,seventies,eighties,nineties,over]
       }]
     },
     options: {scales: {
         xAxes: [{
+           gridLines:{
+           color: gridColor,
+           zeroLineColor : zeroLineColor
+           },
             ticks: {
                 min: 0
             }
+        }],
+       yAxes: [{
+           gridLines:{
+           color: gridColor,
+           zeroLineColor : zeroLineColor
+           }
         }]
     },
       title: {
