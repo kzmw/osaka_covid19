7551290bcc0fa9a06baea54d70b9cd231387d248
kzmw <63456328+kzmw@users.noreply.github.com>
2020-07-25 19:28:53 +0900

    Update json_load.js

--- json_load.js
+++ json_load.js
@@ -66,7 +66,10 @@ fetch( 'https://script.google.com/macros/s/AKfycbyrNVl5HtIe6be_n_d2ixxEG5ABXkTb_
     return response.json();})
    .then(data => {
 $("#number_text").text(data.sum);
-$("#update").text("最終更新：更新停止中");
+if(data.change > 0){$("#change").text("前日比：＋" + data.change + "人")}
+else if(data.change == 0){$("#change").text("前日比：±" + data.change + "人")}
+else if(data.change < 0){$("#change").text("前日比：－" + data.change + "人")}
+$("#update").text("最終更新：" + data.last_update);
 if (data.signal == "yellow") {
           $("#signal_frame").html("<div class=\"signal_off\"></div><div class=\"signal_yerrow\"></div><div class=\"signal_off\"></div>")
         } else if (data.signal == "green") {
