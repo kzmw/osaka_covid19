a7f567d441134ddc3b8eaf08e3158942378389c2
kzmw <63456328+kzmw@users.noreply.github.com>
2020-07-25 19:31:39 +0900

    Update json_load.js

--- json_load.js
+++ json_load.js
@@ -69,7 +69,7 @@ $("#number_text").text(data.sum);
 if(data.change > 0){$("#change").text("前日比：＋" + data.change + "人")}
 else if(data.change == 0){$("#change").text("前日比：±" + data.change + "人")}
 else if(data.change < 0){$("#change").text("前日比：－" + data.change + "人")}
-$("#update").text("最終更新：" + data.last_update);
+$("#update").text("最終更新：" + dateToFormatString(data.last_update, '%YYYY%/%M%/%D% %H%:%mm%:%ss%秒'););
 if (data.signal == "yellow") {
           $("#signal_frame").html("<div class=\"signal_off\"></div><div class=\"signal_yerrow\"></div><div class=\"signal_off\"></div>")
         } else if (data.signal == "green") {
