71e98379197f632cf43251e29a0dddbd2bc1fb76
kzmw <63456328+kzmw@users.noreply.github.com>
2020-08-04 20:49:40 +0900

    Update json_load.js

--- json_load.js
+++ json_load.js
@@ -277,8 +277,7 @@ json2.features[i].properties.proportion = Math.floor(data.city[j].patient / data
 	}
 }}
 }
-	color.domain([min_value, max_value})
-]);
+	color.domain([min_value, max_value]);
 	    var projection2 = d3.geoMercator()
             .scale(scale)
             .center([135.45,34.662])
