83f108452fe88a4c4f55e94e15834db8aefc95f9
kzmw <63456328+kzmw@users.noreply.github.com>
2020-08-15 16:25:38 +0900

    Update json_load.js

--- json_load.js
+++ json_load.js
@@ -84,7 +84,8 @@ if(data.change > 0){$("#change").text("前日比：＋" + data.change + "人")}
 else if(data.change == 0){$("#change").text("前日比：±" + data.change + "人")}
 else if(data.change < 0){$("#change").text("前日比：－" + data.change + "人")}
 var last_update = new Date(data.last_update);
-$("#update").text("最終更新：" + last_update.getFullYear() + "/" + (last_update.getMonth() + 1) + "/" + last_update.getDate() + " " + last_update.getHours() + ":" + last_update.getMinutes());
+var last_update_minute = ('00' + last_update.getMinutes()).slice(-2)
+$("#update").text("最終更新：" + last_update.getFullYear() + "/" + (last_update.getMonth() + 1) + "/" + last_update.getDate() + " " + last_update.getHours() + ":" + last_update_minute);
 if (data.signal == "yellow") {
           $("#signal_frame").html("<div class=\"signal_off\"></div><div class=\"signal_yerrow\"></div><div class=\"signal_off\"></div>")
         } else if (data.signal == "green") {
