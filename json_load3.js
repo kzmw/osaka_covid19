5208ffeaf62bb9a12901ab6a8425ae7f12d1a400
kzmw <63456328+kzmw@users.noreply.github.com>
2021-03-20 12:36:08 +0900

    Update json_load3.js

--- json_load3.js
+++ json_load3.js
@@ -53,10 +53,10 @@ function getJsonp_GAS() {
     .then(data => {
     $("#load_status").text("データを書き出し中");
     $("#number_text").text(data.sum);
-
     var last_update = new Date(data.last_update);
     var last_update_minute = ('00' + last_update.getMinutes()).slice(-2)
     $("#update").text("最終更新：" + last_update.getFullYear() + "/" + (last_update.getMonth() + 1) + "/" + last_update.getDate() + " " + last_update.getHours() + ":" + last_update_minute);
+    $("#change").text("前日比：" + data.dod);
     if (data.signal == "yellow") {
       $("#signal_frame").html("<div class=\"signal_off\"></div><div class=\"signal_yerrow\"></div><div class=\"signal_off\"></div>")
     } else if (data.signal == "green") {
