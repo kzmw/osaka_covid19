09df3717756e48b7b951abf6804036863e8e8238
Kazuma Nishimura <kazumanombiri@icloud.com>
2021-11-15 22:52:32 +0900

    change maxvalue 40

--- json_load3.js
+++ json_load3.js
@@ -173,8 +173,8 @@ function getJsonp_GAS() {
             },
             ticks: {
               min: 0,
-              max: 30,
-              stepSize: 3
+              max: 40,
+              stepSize: 4
             }
           }]
         },
